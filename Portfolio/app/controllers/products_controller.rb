783eca9967ce565cd44680dfe3464e0b4859d2a1
Joungwoo <joungwoo.baik@gmail.com>
2017-12-01 12:33:41 -0800

    Integrated new and create action for products

--- Portfolio/app/controllers/products_controller.rb
+++ Portfolio/app/controllers/products_controller.rb
@@ -2,4 +2,25 @@ class ProductsController < ApplicationController
   def index
     @products_items = Product.all
   end
+
+  def new
+    @product_item = Product.new
+  end
+
+  def create
+    @product_item = Product.new(take_params)
+
+    respond_to do |format|
+      if @product_item.save
+        format.html {redirect_to products_path, notice: "Your portfolio item is now live."}
+      else
+        format.html { render :new}
+      end
+    end
+  end
+end
+
+private
+def take_params
+  params.require(:product).permit(:title, :subtitle, :body)
 end
