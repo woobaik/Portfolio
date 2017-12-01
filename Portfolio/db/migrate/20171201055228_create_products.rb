ce03bed11361dfcd5f704166c5e8c4ffc53bba2d
Joungwoo <joungwoo.baik@gmail.com>
2017-11-30 21:58:42 -0800

    Added product items via Resource generator

---
+++ Portfolio/db/migrate/20171201055228_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
